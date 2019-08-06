c39fc82170f72fd6cc75fc03eb0dea3afb62fa5e
eropf4 <44525878+eropf4@users.noreply.github.com>
2019-08-07 04:18:34 +0500

    Добавил формы. теперь метод выдает series из DataPoint.Чтобы вытащить оттуда точки обращаемся к Series.Points

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using CalculateExpressions.NewExpression;
+﻿using System;
+using CalculateExpressions.NewExpression;
 using CalculateExpressions.BusinessLogic.Expressions;
 using CalculateExpressions.BusinessLogic.Recognitions;
+using System.Windows.Forms.DataVisualization.Charting;
+using ConsoleApp4;
 
 namespace CalculateExpressions.Console
 {
@@ -8,15 +11,20 @@ namespace CalculateExpressions.Console
     {
         static void Main(string[] args)
         {
-            var stringrecognizer = VariedExpression.Change (System.Console.ReadLine());
+            var form = new Form1();
+            var series = new Series();
+            System.Console.WriteLine("start:");var start = Convert.ToInt16( System.Console.ReadLine());
+            System.Console.WriteLine("finish:");var finish = Convert.ToInt16(System.Console.ReadLine());
+            var stringrecognizer = VariedExpression.Change (System.Console.ReadLine(), start, finish);
             foreach (var recognation in stringrecognizer)
             {
                 var recognizer = new Recognizer();
                 var exp = new CalculableExpression(recognizer.Recognize(recognation));
-                System.Console.WriteLine(exp.Result());
+                start += 1;
+                series.Points.Add(new DataPoint(exp.Result(), start));
+                System.Console.WriteLine(series.Points.ToString());
             }
             System.Console.ReadKey();
         }
     }
-
 }
