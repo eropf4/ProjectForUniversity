204cd38e9f689b5857d2ed4d49487e2b1e60e3bd
eropf4 <44525878+eropf4@users.noreply.github.com>
2019-08-15 02:01:47 +0500

    Посоны проект готов, осталось отшлифовать некоторые ошибки и можно сдавать Спиричевой

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StartBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FinishBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExpressionBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // StartBox
+            // 
+            this.StartBox.Location = new System.Drawing.Point(199, 21);
+            this.StartBox.Name = "StartBox";
+            this.StartBox.Size = new System.Drawing.Size(162, 20);
+            this.StartBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Начало графика";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Конец графика";
+            // 
+            // FinishBox
+            // 
+            this.FinishBox.Location = new System.Drawing.Point(199, 47);
+            this.FinishBox.Name = "FinishBox";
+            this.FinishBox.Size = new System.Drawing.Size(162, 20);
+            this.FinishBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(367, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(493, 72);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Функция";
+            // 
+            // ExpressioBox
+            // 
+            this.ExpressionBox.Location = new System.Drawing.Point(199, 73);
+            this.ExpressionBox.Name = "ExpressionBox";
+            this.ExpressionBox.Size = new System.Drawing.Size(162, 20);
+            this.ExpressionBox.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(875, 572);
+            this.Controls.Add(this.ExpressionBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FinishBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartBox);
             this.Name = "Form1";
             this.Text = "ProjectEPM";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox StartBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FinishBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ExpressionBox;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using CalculateExpressions.NewExpression;
-using CalculateExpressions.BusinessLogic.Expressions;
-using CalculateExpressions.BusinessLogic.Recognitions;
-using System.Windows.Forms.DataVisualization.Charting;
+
 using GraphInterface;
 using System.Drawing;
 using System.Windows.Forms;
@@ -11,35 +8,10 @@ namespace CalculateExpressions.Console
 {
     class Program
     {
-        
-        static void Main(string[] args)
+
+        static void Main()
         {
-            var series = new Series();
-            System.Console.WriteLine("start:");var start = Convert.ToInt16( System.Console.ReadLine());
-            System.Console.WriteLine("finish:");var finish = Convert.ToInt16(System.Console.ReadLine());
-            var stringrecognizer = VariedExpression.Change (System.Console.ReadLine(), start, finish);
-            foreach (var recognation in stringrecognizer)
-            {
-                var recognizer = new Recognizer();
-                var exp = new CalculableExpression(recognizer.Recognize(recognation));
-                series.Points.Add(new DataPoint(start,exp.Result() ));
-                start += 1;
-                System.Console.WriteLine(series.Points.ToString());
-            }
-
-            var chart = new Chart();
-            chart.ChartAreas.Add(new ChartArea());
-
-            series.ChartType = SeriesChartType.FastLine;
-            series.Color = Color.Red;
-            series.MarkerBorderWidth = 3;
-
-            chart.Series.Add(series);
-            chart.Dock = DockStyle.Fill;
-
-            var form = new Form1();
-            form.Controls.Add(chart);
-            Application.Run(form);
+            Application.Run(new Form1());
         }
     }
 }
